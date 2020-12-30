919adbce18bc17370f6d2b9e160e28ea0fc11675
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 18:26:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,6 +44,13 @@ def monopoly_with_third_tier
    
   hash[:railroads][:names] = {}
    
+  hash[:railroads][:rent_in_dollars] = {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
+  }
+   
   hash
 
 end
