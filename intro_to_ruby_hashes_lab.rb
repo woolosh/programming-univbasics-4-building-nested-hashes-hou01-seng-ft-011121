6217215d355544b0bca00bfbe1d4c1582e4702ae
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 18:31:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,10 +29,6 @@ end
 
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   
     hash = {
     :railroads => {}
@@ -64,5 +60,27 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+    
+    hash = {
+    :railroads => {}
+  } 
+  
+  hash[:railroads][:pieces] = 4
+   
+  hash[:railroads][:rent_in_dollars] = {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
+  }
+  
+   hash[:railroads][:names] = {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
+   }
+   
+  hash
 
 end
