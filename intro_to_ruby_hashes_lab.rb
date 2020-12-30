21794dbe225be576f770ed7882b39c82cbfac07e
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 18:10:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,8 @@ def monopoly_with_second_tier
     :railroads => {}
   } 
   
-   hash[:railroads][:pieces] = 4
-   [:rent_in_dollars] = {}
+   hash[:railroads][:pieces] = 4,
+   [:rent_in_dollars] = {},
    [:names] = {}
    
    hash
